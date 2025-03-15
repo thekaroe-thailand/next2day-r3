ae2f47b619787c98dfb53d359676d2a0a80f107c
thekaroe-thailand <thekaroe@gmail.com>
2025-03-15 14:56:39 +0700

    my code

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,9 +4,18 @@ export default function Sidebar() {
     return (
         <div className="sidebar">
             <div className="flex flex-col gap-2">
-                <Link href="/">Home</Link>
-                <Link href="/http">HTTP</Link>
-                <Link href="/settings/user">User</Link>
+                <Link href="/">
+                    <i className="fa fa-home mr-2"></i>
+                    Home
+                </Link>
+                <Link href="/http">
+                    <i className="fa fa-server mr-2"></i>
+                    HTTP
+                </Link>
+                <Link href="/settings/user">
+                    <i className="fa fa-user mr-2"></i>
+                    User
+                </Link>
             </div>
         </div>
     )
